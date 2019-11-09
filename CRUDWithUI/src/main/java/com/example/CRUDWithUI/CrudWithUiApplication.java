ba170e5cc94cca010c3bfb38614695888b06f113
hnrami <hemang@omedix.com>
2019-11-09 23:15:08 +0530

    First HelloWord
    First HelloWord

---
+++ CRUDWithUI/src/main/java/com/example/CRUDWithUI/CrudWithUiApplication.java
@@ -0,0 +1,14 @@
+package com.example.CRUDWithUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithUiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(CrudWithUiApplication.class, args);
+	}
+
+}
